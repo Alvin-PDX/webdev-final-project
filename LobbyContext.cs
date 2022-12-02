38fc23b43ae418ef9950934ee47064dd88fd7af2
JohnN <66976803+xiivi@users.noreply.github.com>
2022-12-02 12:48:04 -0800

    Delete LobbyContext.cs

--- LobbyContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-namespace Connect4_API.Models;
-
-public class LobbyContext : DbContext
-{ 
-    public LobbyContext(DbContextOptions<LobbyContext> options) : base(options) { }
-
-    public DbSet<Lobby>Lobby { get; set; } = null!;
-}
